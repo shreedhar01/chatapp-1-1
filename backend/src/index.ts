8f284e1fd52eb7ce63829cd4cd245d2024388681
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-02 14:25:05 +0545

    init: express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+import express from "express"
+
+const app = express()
+
+app.get("/",(_,res)=>{
+    res.send("Backend is up and running")
+})
+
+app.listen(process.env.PORT,()=>{
+     console.log("Listening on port ",process.env.PORT)
+})
