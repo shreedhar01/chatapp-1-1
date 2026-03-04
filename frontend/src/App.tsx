537671bdf8cbf010950e08e9016bdc6d0945dd73
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-04 19:25:22 +0545

    feat: react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages/Home"
+
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const HomePage = ()=>{
+    return (
+        <div className="h-screen w-full">
+            Home Page
+        </div>
+    )
+}
