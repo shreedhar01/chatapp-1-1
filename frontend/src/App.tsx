9a8e31bc22c4ad21c8a98d4dce5412a3f8ffab92
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-04 20:14:36 +0545

    feat: theme toggler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { HomePage } from "./pages/Home"
+import { ThemeProvider } from "@/components/theme-provider"
 
 
 function App() {
   return (
+    <ThemeProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <HomePage/>}/>
       </Routes>
     </BrowserRouter>
+
+    </ThemeProvider>
   )
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,12 @@
-export const HomePage = ()=>{
+import { ModeToggle } from "@/components/mode-toggle"
+
+export const HomePage = () => {
     return (
-        <div className="h-screen w-full">
-            Home Page
+        <div className="h-screen w-full p-4">
+            <div className="flex w-full justify-between items-center  ">
+                <p className="font-medium text-xl">Let's Chat</p>
+                <ModeToggle />
+            </div>
         </div>
     )
 }
