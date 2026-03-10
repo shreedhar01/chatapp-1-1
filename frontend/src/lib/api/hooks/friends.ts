d2184bbc498ebcabc7039a0437b4bd838ee470e9
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-10 18:05:26 +0545

    style: clean up

--- frontend/src/lib/api/hooks/friends.ts
+++ frontend/src/lib/api/hooks/friends.ts
@@ -1,35 +1,6 @@
-import { useInfiniteQuery, useMutation } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { api } from "../axios";
-import axios from "axios";
-import type { FriendData, SearchFriend } from "@/schema/friend.schema";
-
-// export function useSearchFriends() {
-//     return useMutation({
-//         mutationKey: ["searchFriends"],
-//         mutationFn: async (input:SearchFriend):Promise<FriendData> => {
-//             const response = await api.post("/friend/search", input,{
-//                 params:{
-//                     page : 1,
-//                     limit : 20
-//                 }
-//             })
-//             return response.data.data as FriendData
-//         },
-//         onSuccess: (data) => {
-//             // console.log(data.data)
-//         },
-//         onError: (err) => {
-//             if (axios.isAxiosError(err)) {
-//                 const message = err.response?.data?.message ?? "Something went wrong"
-//                 console.log("Error while searching friends :: ", message)
-//                 // toast.error(message)
-//             } else {
-//                 // toast.error("Unexpected error occurred")
-//                 console.log("Unexpected error occurred")
-//             }
-//         }
-//     })
-// }
+import type { FriendData } from "@/schema/friend.schema";
 
 
 export function useSearchFriends(name: string) {
