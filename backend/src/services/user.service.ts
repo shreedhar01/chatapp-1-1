179e3f393846cc04ce4787522045d5b1d52431af
shreedhar01 <shreedharkalamagar@gmail.com>
2026-03-18 19:52:34 +0545

    fix: login password compare, update db field and logout update db field

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -3,7 +3,7 @@ import { db } from "../db/index.js";
 import { users } from "../db/schema.js";
 import type { LoginUser, RegisterUser } from "../types/dbLevel.types.js";
 import { ApiError } from "../utils/ApiError.js";
-import { hashPassword } from "../utils/bcrypt.js";
+import { comparePassword, hashPassword } from "../utils/bcrypt.js";
 import { signJwt } from "../utils/jwt.js";
 
 export const registerUserService = async (data: RegisterUser) => {
@@ -36,10 +36,23 @@ export const registerUserService = async (data: RegisterUser) => {
 
 
 export const loginUserService = async (data: LoginUser) => {
-    const [isUserExist] = await db.select().from(users).where(eq(users.email, data.email));
+    const [isUserExist] = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, data.email));
     if (!isUserExist) {
         throw new ApiError(400, "User doesn't exist")
     }
+
+    const isPasswordTrue = await comparePassword(data.password, isUserExist.password)
+    if (!isPasswordTrue) {
+        throw new ApiError(400, "Password don't match")
+    }
+
+    await db
+        .update(users)
+        .set({ status: "active",last_seen: new Date() })
+        .where(eq(users.id, isUserExist.id))
 
     const token = await signJwt({
         id: isUserExist.id.toString(),
@@ -56,6 +69,6 @@ export const loginUserService = async (data: LoginUser) => {
 
 export const logoutUserService = async (data: number) => {
     await db.update(users)
-        .set({ status: "offline" })
+        .set({ status: "offline", last_seen: new Date() })
         .where(eq(users.id, data));
 }
