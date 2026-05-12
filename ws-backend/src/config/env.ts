c2f2bea2385426da65defa6e6c1f1591953783d0
shreedhar01 <shreedharkalamagar@gmail.com>
2026-05-12 16:02:57 +0545

    feat: dynamic ws-backend cors

--- ws-backend/src/config/env.ts
+++ ws-backend/src/config/env.ts
@@ -2,5 +2,6 @@ export const config = {
     PORT:`${process.env.PORT}`,
     REST_API_BACKEND:`${process.env.REST_API_BACKEND}`,
     JWT_SECRET:`${process.env.JWT_SECRET}`,
-    JWT_LIFE:`${process.env.JWT_LIFE}`
+    JWT_LIFE:`${process.env.JWT_LIFE}`,
+    FRONTEND_URL:`${process.env.FRONTEND_URL}`,
 }
--- ws-backend/src/index.ts
+++ ws-backend/src/index.ts
@@ -8,7 +8,7 @@ const httpServer = createServer()
 const io = new Server(httpServer,{
     cors: {
         origin: [
-            "http://localhost:5173"
+            config.FRONTEND_URL
         ],
         credentials: true
     }
